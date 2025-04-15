c4476a26f95d44222c7658946dfcc4c9d57a11f5
rami <rami.kaldany@pm.me>
2025-04-15 03:05:54 -0400

    Fixes to allocator

--- src/arch/i686/allocator.c
+++ src/arch/i686/allocator.c
@@ -13,7 +13,7 @@ void alloc_init(void) {
     ASSERT(ctx.mem_start < 0x400000,
            "Bitmap must start before 0x400000 to avoid temporary page tables");
 
-    bitmap_size = ctx.mem_len / 0x1000 / 8;
+    bitmap_size = ctx.mem_len / 0x1000;
 
     ASSERT(ctx.mem_start + bitmap_size <= 0x400000,
            "Bitmap can't surpass 0x400000 to avoid temporary page tables");
@@ -30,15 +30,17 @@ void alloc_init(void) {
 uint8_t *alloc_get_byte(uint32_t page_num) {
     ASSERT(page_num < bitmap_size, "Trying to access an invalid page");
     const uint32_t byte_num = page_num / 8;
-    uint8_t *byte = (uint8_t *)(bitmap_start + byte_num);
+    const uint32_t addr = bitmap_start + byte_num;
+    uint8_t *byte = (uint8_t *)addr;
     return byte;
 }
 
 void alloc_free_page(uint32_t page_num) {
-    uint8_t *byte = alloc_get_byte(page_num);
     const uint32_t bit_num  = page_num % 8;
-    const uint8_t mask = 1 << bit_num;
-    *byte ^= mask;
+    const uint8_t mask = ~(1 << bit_num);
+    uint8_t *byte = alloc_get_byte(page_num);
+    ASSERT(byte, "Nullptr");
+    *byte &= mask;
 }
 
 void alloc_res_page(uint32_t page_num) {
@@ -54,7 +56,7 @@ void *alloc_first_page(void) {
     for (uint32_t i = 0; i < bitmap_size; i++) {
         uint8_t *byte = alloc_get_byte(i);
         const uint32_t bit_num = i % 8;
-        if (BIT(*byte, bit_num))
+        if (!BIT(*byte, bit_num))
             return (void *)(i * 0x1000);
     }
 
