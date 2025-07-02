83bd3205bcfcbf1a0e9aa5bfc843ad0aedf3d0e5
Rami Kaldany <ramiik@umich.edu>
2025-07-02 18:09:19 -0400

    Add comments to interrupts.h

--- src/arch/i686/interrupts.c
+++ src/arch/i686/interrupts.c
@@ -1,17 +1,8 @@
 #include <interrupts.h>
 #include <pic.h>
 
-extern uint32_t isr_stub_table[32];
 struct idt_entry idt[IDT_MAX];
 
-/**
- * @brief Encodes an entry in the Interrupt Descriptor Table (IDT).
- *
- * @param i The index of the IDT entry to encode.
- * @param offset The 32-bit offset of the interrupt service routine.
- * @param segment_selector The segment selector for the interrupt gate.
- * @param attributes The attributes for the IDT entry, specifying gate type and privilege level.
- */
 void int_encode_idt(int i, uint32_t offset, uint16_t segment_selector, uint8_t attributes) {
     idt[i].offset_lo = (uint16_t)(offset & 0xFFFF);
     idt[i].seg_select = segment_selector;
@@ -20,18 +11,6 @@ void int_encode_idt(int i, uint32_t offset, uint16_t segment_selector, uint8_t a
     idt[i].offset_hi = (uint16_t)(offset >> 16);
 }
 
-extern void pit_isr();
-extern void syscall_isr();
-
-
-/**
- * @brief Initializes the IDT and enables interrupts.
- *
- * This function defines:
- * - CPU exceptions (0-31) with trap gates at ring 0.
- * - The PIT interrupt (0x20) with interrupt gates at ring 0.
- * - The system call interrupt (0x80) with interrupt gates at ring 3.
- */
 void int_init() {
     for (int i = 0; i < 32; i++) {
         int_encode_idt(i, isr_stub_table[i], 0x8, INT_PRESENT | TRAP_GATE_32 | INT_RING0);
--- src/arch/i686/interrupts.h
+++ src/arch/i686/interrupts.h
@@ -4,7 +4,7 @@
 #include <stdint.h>
 
 #define IDT_MAX 256
-
+#define EXCEPTION_COUNT 32
 #define ENTER_CRITICAL() asm volatile("cli" ::)
 #define EXIT_CRITICAL() asm volatile("sti" ::)
 
@@ -36,8 +36,40 @@ enum IDT_ATTRIBUTES {
     INT_PRESENT = 0x80,
 };
 
+/**
+ * @brief A lookup table containing addresses to the ISRs for the 32 CPU exceptions, from start.S
+ */
+extern uint32_t isr_stub_table[EXCEPTION_COUNT];
+
+/**
+ * @brief Initialize the IDT. Handle CPU exceptions (0-31), the system timer interrupt (0x20), and the syscall interrupt (0x80)
+ */
 void int_init();
+
+/**
+ * @brief Encodes an entry in the Interrupt Descriptor Table (IDT)
+ *
+ * @param i The index of the IDT entry to encode
+ * @param offset The 32-bit offset of the interrupt service routine
+ * @param segment_selector The segment selector for the interrupt gate
+ * @param attributes The attributes for the IDT entry, specifying gate type and privilege level
+ */
 void int_encode_idt(int i, uint32_t offset, uint16_t segment_selector, uint8_t attributes);
+
+/**
+ * @brief Initializes the Programmable Interrupt Controller (PIC)
+ */
 void int_pic_init();
+
+/**
+ * @brief Interrupt Service Routine (ISR) for the PIT, implemented in start.S
+ */
+extern void pit_isr();
+
+/**
+ * @brief Interrupt Service Routine (ISR) for syscalls, implemented in start.S
+ */
+extern void syscall_isr();
+
 
 #endif
