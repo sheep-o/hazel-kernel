11b2c965b91f87a453cb0b0e0782b4c247ac0d20
Rami Kaldany <ramiik@umich.edu>
2025-07-02 17:54:28 -0400

    Comments for exception.h

--- src/arch/i686/exception.h
+++ src/arch/i686/exception.h
@@ -3,6 +3,9 @@
 
 #include <stdint.h>
 
+/**
+ * @brief Represents the CPU's state at the time of the exception
+ */
 struct ex_frame {
     uint32_t edi, esi, ebp, esp, ebx, edx, ecx, eax; /*  Pushed by pusha. */
     uint32_t n, err;    /*  Interrupt number and error code (if applicable) */
@@ -12,6 +15,11 @@ struct ex_frame {
     uint32_t eflags;
 };
 
+/**
+ * @brief Handles CPU exceptions by printing out the CPU state and halting
+ * @param r CPU state
+ * @todo Allow for recovery from certain exceptions like page faults
+ */
 void exception_handler(struct ex_frame r);
 
 #endif
